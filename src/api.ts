c315835c498ce84d157b3d1929758ba26283c603
Yonny Zohar <yonnyzohar@gmail.com>
2026-01-17 21:37:58 +0000

    Fix API_BASE URL by removing trailing slash

--- src/api.ts
+++ src/api.ts
@@ -19,7 +19,7 @@ export interface User {
     email: string;
 }
 
-const API_BASE = 'https://zstudiolicenseserver.onrender.com/';//'http://127.0.0.1:4000';
+const API_BASE = 'https://zstudiolicenseserver.onrender.com';//'http://127.0.0.1:4000';
 
 //login and sign up. -get jwt token and user id
 
